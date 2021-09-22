3dc8cd5c6a5503769f7e862e2aa54b2b2375e2a9
Anmol Sharma <anmolsh.1908@gmail.com>
2021-09-22 13:38:25 +0530

    setup cors, helmet, express json and urlencoded

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
 import  Express  from "express";
+import Cors from "cors";
+import Helmet from "helmet";
+
 const Zomato = Express();
+
+Zomato.use(Express.urlencoded({extended: false}));
+Zomato.use(Express.json());
+Zomato.use(Cors());
+Zomato.use(Helmet());
 
 Zomato.get("/", (req, res) => {
     res.json({message: "Success!"})
