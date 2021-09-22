41dd4cd5dab25365a35ed53c70035e384dd862bb
Anmol Sharma <anmolsh.1908@gmail.com>
2021-09-22 16:35:47 +0530

    Food Model

---
+++ server/Database/Foods/index.js
@@ -0,0 +1,21 @@
+import { Mongoose } from "mongoose";
+
+const FoodSchema = new Mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  isVeg: { type: Boolean, required: true },
+  containsEgg: { type: Boolean, required: true },
+  category: { type: String, required: true },
+  photos: [{
+      type: Mongoose.Types.ObjectId,
+      ref: "Images"
+  }],
+  price: {type: Number, required: true},
+  addOns: [{
+      type: Mongoose.Types.ObjectId,
+      ref: "Foods"
+  }],
+  restaurant: {type: Mongoose.Types.ObjectId, ref: "Restaurants", required: true}
+});
+
+export const FoodModel = Mongoose.model("Foods", FoodSchema);
