e4894f5a3ff21f62e68c00005f5ae1739de65987
Anmol Sharma <anmolsh.1908@gmail.com>
2021-09-27 11:24:38 +0530

    database connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
+// libraries
 import  Express  from "express";
 import Cors from "cors";
 import Helmet from "helmet";
-import dotenv from "dotenv";
+
+// env
+require("dotenv").config();
+
+
+//database connection
+import ConnectDB from "./Database/connection";
+
 
 const Zomato = Express();
-const envConfig = dotenv.config();
 
 Zomato.use(Express.urlencoded({extended: false}));
 Zomato.use(Express.json());
@@ -15,4 +22,10 @@ Zomato.get("/", (req, res) => {
     res.json({message: "Success!"})
 })
 
-Zomato.listen(3000, () => console.log("Server is running"))
+Zomato.listen(3000, () => console.log("Server is running")).then(()=>{
+    ConnectDB()
+      .then(() => console.log("Database is connected!"))
+      .catch(() => console.log("Database connection failed!"));
+})
+
+
