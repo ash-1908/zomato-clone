03285b1854b56484e088f96d9f4d6ec022231fac
Anmol Sharma <anmolsh.1908@gmail.com>
2021-10-03 15:12:46 +0530

    added sign in route

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -1,7 +1,6 @@
 //libraries
 import Express from "express";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 
 //
 const Router = Express.Router();
@@ -34,6 +33,33 @@ Router.post("/signup", async (req, res) => {
   } catch (error) {
       return res.status(500).json({ error: error.message });
     }
+});
+
+/* 
+Route           /signin
+Description     Signin with email and password
+Params          None
+Access          Public
+Method          POST
+*/
+Router.post("/signin", async (req, res) => {
+  const { email, password } = req.body.credentials;
+  const user = await UserModel.findOne({ email });
+  //check if the user exists
+  if(user) {
+    //check password
+    bcrypt.compare(password, user.password, (error, res) => {
+      if(error)
+        throw new Error(error);
+      if(!res)
+        throw new Error("Invalid password");
+    });
+    const token = user.genJwtToken();
+    res.status(200).json({message: "Success", token: token});
+  }
+  else {
+    res.status(201).json({Failed: "User doesn't exists"});
+  }
 });
 
 export default Router;
