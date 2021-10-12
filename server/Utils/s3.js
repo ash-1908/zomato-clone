481341a6c2317f7942a9e7826a5f7ee9c2e478ea
Anmol Sharma <anmolsh.1908@gmail.com>
2021-10-12 16:37:12 +0530

    solved bug: invalid credentials in AWS

--- server/Utils/s3.js
+++ server/Utils/s3.js
@@ -1,10 +1,11 @@
 import AWS from "aws-sdk";
+
 require("dotenv").config();
 
 //AWS S3 bucket config
 const bucket = new AWS.S3({
-  accessKeyId: process.env.AWS_S3_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_S3_ACCESS_SECRET,
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_ACCESS_SECRET,
   region: "ap-south-1",
 });
 
