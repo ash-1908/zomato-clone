28ca31af83216cb181734404e1ea536a72942694
Anmol Sharma <anmolsh.1908@gmail.com>
2022-04-03 12:05:52 +0530

    tweaked restaurant card component

--- client/src/Components/Delivery/RestaurantCard.jsx
+++ client/src/Components/Delivery/RestaurantCard.jsx
@@ -7,18 +7,22 @@ const RestaurantCard = (props) => {
 		<div className="w-full md:w-1/2 lg:w-1/3 hover:shadow-xl rounded-xl p-2 my-5 border border-transparent transition ease-in-out duration-400 hover:border-gray-200">
 			<div className="relative w-full h-56 xl:h-64">
 				<div className="absolute z-10 flex justify-between w-full items-end bottom-0 mb-2 text-sm text-white font-normal">
-          <div className="flex flex-col">
-            <span className="bg-zred-500 px-1 rounded-sm mb-3">
-              Pro extra 60% OFF
-            </span>
-            <span className="bg-blue-500 px-1 rounded-sm">
-              60% OFF
-            </span>
-          </div>
-          <span className="bg-gray-100 text-gray-600 px-1 rounded mr-3 text-xs py-0.5">52 min</span>
-        </div>
+					<div className="flex flex-col">
+						{props.proOffer && (
+							<span className="bg-zred-500 px-1 rounded-sm mb-3">
+								{props.proOffer}
+							</span>
+						)}
+						{props.offer && (
+							<span className="bg-blue-500 px-1 rounded-sm">
+								{props.offer}
+							</span>
+						)}
+					</div>
+					<span className="bg-gray-100 text-gray-600 px-1 rounded mr-3 text-xs py-0.5">{props.deliveryDuration} min</span>
+				</div>
 				<img
-					src={props.photos[0]}
+					src={props.photos.length && props.photos[0]}
 					alt="restaurant image"
 					className="w-full h-full object-cover rounded-xl relative"
 				/>
--- client/src/Components/Delivery/index.jsx
+++ client/src/Components/Delivery/index.jsx
@@ -14,6 +14,8 @@ const Delivery = () => {
 			restaurantreviewrating: 3.8,
 			cuisine: ["Mithai", "Deserts", "Bakery"],
 			averageCost: 300,
+			offer: "60% OFF",
+			deliveryDuration: 54
 		},
     {
 			name: "Bhawna Sweets",
@@ -23,6 +25,8 @@ const Delivery = () => {
 			restaurantreviewrating: 3.8,
 			cuisine: ["Mithai", "Deserts", "Bakery"],
 			averageCost: 300,
+			offer: "10% OFF",
+			deliveryDuration: 34
 		},
     {
 			name: "Bhawna Sweets",
@@ -32,7 +36,32 @@ const Delivery = () => {
 			restaurantreviewrating: 3.8,
 			cuisine: ["Mithai", "Deserts", "Bakery"],
 			averageCost: 300,
+			offer: "BUY 1 GET 1",
+			deliveryDuration: 23
 		},
+		{
+			name: "Bhawna Sweets",
+			photos: [
+				"https://b.zmtcdn.com/data/pictures/9/3401239/65840c3566f0e9a6cc7d265834f19cce.jpeg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+			],
+			restaurantreviewrating: 3.8,
+			cuisine: ["Mithai", "Deserts", "Bakery"],
+			averageCost: 300,
+			offer: "20% OFF",
+			deliveryDuration: 44
+		},
+		{
+			name: "Bhawna Sweets",
+			photos: [
+				"https://b.zmtcdn.com/data/pictures/9/3401239/65840c3566f0e9a6cc7d265834f19cce.jpeg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+			],
+			restaurantreviewrating: 3.8,
+			cuisine: ["Mithai", "Deserts", "Bakery"],
+			averageCost: 300,
+			proOffer: "Pro extra 60% OFF",
+			offer: "60% OFF",
+			deliveryDuration: 54
+		}
 	]);
 	return (
 		<>
