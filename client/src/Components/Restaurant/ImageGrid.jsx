cc169f14f85d0d0d29c5191faabf2c2959813c96
Anmol Sharma <anmolsh.1908@gmail.com>
2022-04-27 09:37:37 +0530

    Restaurant page image grid

--- client/src/Components/Restaurant/ImageGrid.jsx
+++ client/src/Components/Restaurant/ImageGrid.jsx
@@ -11,36 +11,36 @@ const ImageGrid = (props) => {
 			</div>
 
 			<div className='hidden md:flex w-full h-96 justify-center mt-2 cursor-pointer'>
-				<div className='w-7/12 h-full mr-1 overflow-hidden'>
+				<div className='w-full h-full mr-1 overflow-hidden'>
 					<img
 						src={props.images[0]}
 						alt='Restaurant image'
-						className='w-full h-full rounded-sm transform hover:scale-110 transition duration-700'
+						className='w-full h-full rounded-md transform hover:scale-110 transition duration-700'
 					/>
 				</div>
-				<div className='w-1/6 flex flex-col mr-1 overflow-hidden'>
+				<div className='w-1/4 flex flex-col mr-1 overflow-hidden'>
 					<img
 						src={props.images[1]}
 						alt='Restaurant image'
-						className='w-full h-full mb-1 rounded-sm transform hover:scale-110 transition duration-700'
+						className='w-full h-full mb-1 rounded-md transform hover:scale-110 transition duration-700'
 					/>
 					<img
 						src={props.images[2]}
 						alt='Restaurant image'
-						className='w-full h-full rounded-sm transform hover:scale-110 transition duration-700'
+						className='w-full h-full rounded-md transform hover:scale-110 transition duration-700'
 					/>
 				</div>
-				<div className='w-1/6 flex flex-col overflow-hidden'>
+				<div className='w-1/4 flex flex-col overflow-hidden'>
 					<div className='w-full h-full relative mb-1'>
-						<img src={props.images[3]} alt='Restaurant image' className='w-full h-full rounded-sm' />
-						<div className='absolute inset-0 bg-black bg-opacity-50 rounded-sm' />
+						<img src={props.images[3]} alt='Restaurant image' className='w-full h-full rounded-md' />
+						<div className='absolute inset-0 bg-black bg-opacity-50 rounded-md' />
 						<h4 className='absolute text-white top-20 left-0 text-center w-full h-full text-lg font-semibold'>
 							View Gallery
 						</h4>
 					</div>
 					<div className='w-full h-full relative'>
-						<img src={props.images[4]} alt='Restaurant image' className='w-full h-full rounded-sm' />
-						<div className='absolute inset-0 bg-gray-400 bg-opacity-80 rounded-sm' />
+						<img src={props.images[4]} alt='Restaurant image' className='w-full h-full rounded-md' />
+						<div className='absolute inset-0 bg-gray-400 bg-opacity-80 rounded-md' />
 						<div className='absolute text-white top-1/3 left-0 text-center w-full h-full text-md items-center flex flex-col'>
 							<MdOutlineCameraEnhance className='text-4xl bg-black bg-opacity-50 rounded-2xl p-1' />
 							<h4 className='font-bold'>Add Photos</h4>
--- client/src/Layout/Restaurant.Layout.jsx
+++ client/src/Layout/Restaurant.Layout.jsx
@@ -4,6 +4,9 @@ import React from 'react'
 import RestaurantNavbar from '../Components/Restaurant/Navbar'
 import ImageGrid from '../Components/Restaurant/ImageGrid'
 
+//icons
+import {AiFillStar} from 'react-icons/ai'
+
 const RestaurantLayout = () => {
   return (
 	<>
@@ -12,6 +15,15 @@ const RestaurantLayout = () => {
 		</header>
 		<div className="container mx-auto px-5 xl:px-40">
 			<ImageGrid images={["https://th.bing.com/th/id/OIP.ejj0IBWHM_RdyZIzwBN0-QHaEz?pid=ImgDet&rs=1", "https://th.bing.com/th/id/OIP.ejj0IBWHM_RdyZIzwBN0-QHaEz?pid=ImgDet&rs=1", "https://th.bing.com/th/id/OIP.ejj0IBWHM_RdyZIzwBN0-QHaEz?pid=ImgDet&rs=1", "https://th.bing.com/th/id/OIP.ejj0IBWHM_RdyZIzwBN0-QHaEz?pid=ImgDet&rs=1", "https://th.bing.com/th/id/OIP.ejj0IBWHM_RdyZIzwBN0-QHaEz?pid=ImgDet&rs=1"]} />
+			<div>
+				<h1 className='text-4xl font-semibold'>
+					Rahil Restaurant
+				</h1>
+				<div>
+					<span>3.8 <AiFillStar /> </span>
+				</div>
+			</div>
+		
 		</div>
 	</>
   )
