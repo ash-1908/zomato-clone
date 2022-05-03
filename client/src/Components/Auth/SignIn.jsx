3b56726b9a0a780249386424c785518ef7fb04b5
Anmol Sharma <anmolsh.1908@gmail.com>
2022-05-03 21:20:41 +0530

    Sign In/Up Modal

---
+++ client/src/Components/Auth/SignIn.jsx
@@ -0,0 +1,95 @@
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react'
+
+//icons
+import {FcGoogle} from 'react-icons/fc'
+
+export default function SignIn({isOpen, setIsOpen}) {
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  return (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-2xl font-medium leading-6 text-gray-900"
+                  >
+                    Sign In
+                  </Dialog.Title>
+                  <div className="mt-6 flex flex-col items-start">
+                    <div className='w-full'>
+                      <button 
+                      className='w-full px-3 py-2 border border-gray-200 rounded-lg hover:bg-zred-100 hover:text-zred-600'>
+                        <span className='flex items-center gap-2 w-full justify-center '>Sign In with Google <FcGoogle /></span></button>
+                    </div>
+                    <hr className='w-full my-6' />
+                    <div className='w-full'>
+                      <form action="" className='w-full flex flex-col gap-3 items-start'>
+                        <div className='flex flex-col gap-1 w-full'>
+                          <label htmlFor="email" className=''>Email</label>
+                          <input 
+                          type="email" 
+                          id="email" 
+                          name="email" 
+                          className='w-full focus:bg-white bg-gray-100 px-2 h-8 border rounded-lg focus:outline-none'
+                          placeholder='example@example.com'/>
+                        </div>
+                        <div className='flex flex-col gap-1 items-start w-full'>
+                          <label htmlFor="password">Password</label>
+                          <input 
+                          type="password" 
+                          id="password" 
+                          name="password"
+                          className='w-full focus:bg-white bg-gray-100 px-2 h-8 border rounded-lg focus:outline-none'
+                          placeholder='password'/>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+
+                  <div className="mt-4 text-center">
+                    <button
+                      type="button"
+                      className="inline-flex border border-gray-200 justify-center rounded-md border border-transparent hover:bg-zred-100 px-4 py-2 text-sm font-medium hover:text-zred-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={closeModal}
+                    >
+                      Sign In
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  )
+}
--- client/src/Components/Navbar/index.jsx
+++ client/src/Components/Navbar/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 //icons
 import { FaUserAlt } from "react-icons/fa";
@@ -6,7 +6,12 @@ import { MdLocationOn } from "react-icons/md";
 import { HiChevronDown } from "react-icons/hi";
 import { BiSearchAlt } from "react-icons/bi";
 
-const MobileNav = () => {
+import SignIn from "../Auth/SignIn";
+import SignUp from "../Auth/SignUp"
+
+const MobileNav = ({signIn,signUp }) => {
+	const [isDropdownOpen, setIsDropDownOpen] = useState(false);
+
 	return (
 		<div className="flex justify-between items-center">
 			<div className="w-24 px-0.5 md:w-28">
@@ -20,15 +25,26 @@ const MobileNav = () => {
 				<button className="bg-zred-400 text-white text-sm px-3 py-2 rounded-full">
 					Use App
 				</button>
-				<span className="border border-gray-300 text-zred-400 rounded-full p-2 ml-2">
+				<div 
+				className="border cursor-pointer border-gray-300 text-zred-400 rounded-full p-2 ml-2 relative h-8 w-8"
+				onClick={() => setIsDropDownOpen((prev) => !prev)}>
+					{
+						isDropdownOpen && (
+							<div className="absolute w-24 shadow-lg text-center flex flex-col items-start right-0 top-10 z-10 bg-white rounded-lg border border-gray-400">
+								<button className="w-full hover:bg-zred-50 py-2" onClick={signIn}>Sign In</button>
+								<hr className="w-full " />
+								<button className="w-full hover:bg-zred-50 py-2" onClick={signUp}>Sign Up</button>
+							</div>
+						)
+					}
 					<FaUserAlt />
-				</span>
+				</div>
 			</div>
 		</div>
 	);
 };
 
-const LgNav = () => {
+const LgNav = ({signIn, signUp}) => {
 	return (
 		<div className="w-full flex items-center justify-between py-3">
 			<div className="w-3/4 flex items-center">
@@ -66,10 +82,10 @@ const LgNav = () => {
 				</div>
 			</div>
 			<div className="flex items-center justify-end w-1/4">
-				<button className="text-lg text-gray-400 hover:text-gray-600 cursor:pointer mr-4">
+				<button className="text-lg text-gray-400 hover:text-gray-600 cursor:pointer mr-4" onClick={signIn}>
 					Log In
 				</button>
-				<button className="text-lg text-gray-400 hover:text-gray-600 cursor:pointer ml-4">
+				<button className="text-lg text-gray-400 hover:text-gray-600 cursor:pointer ml-4" onClick={signUp}>
 					Sign Up
 				</button>
 			</div>
@@ -78,14 +94,22 @@ const LgNav = () => {
 };
 
 const Navbar = () => {
+	const [isOpenSignIn, setisOpenSignIn] = useState(false);
+	const [isOpenSignUp, setisOpenSignUp] = useState(false);
+
+	const openSignIn = () => setisOpenSignIn(true);
+	const openSignUp = () => setisOpenSignUp(true);
+
 	return (
 		<>
+			<SignIn isOpen={isOpenSignIn} setIsOpen={setisOpenSignIn} />
+			<SignUp isOpen={isOpenSignUp} setIsOpen={setisOpenSignUp} />
 			<nav className="">
 				<div className="p-4 bg-white shadow-md lg:hidden">
-					<MobileNav />
+					<MobileNav signIn={openSignIn} signUp={openSignUp} />
 				</div>
 				<div className="hidden lg:block">
-					<LgNav />
+					<LgNav signIn={openSignIn} signUp={openSignUp} />
 				</div>
 			</nav>
 		</>
